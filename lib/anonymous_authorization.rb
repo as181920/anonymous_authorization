aa38832587b96023a67e844c0ea4628610f5c996
Andersen Fan <as181920@gmail.com>
2024-01-29 22:13:27 +0800

    Refactor: set table_name_prefix before calling isolate_namespace
    fix table_name_prefix lost after upgrade Rails to v7.1.3

--- lib/anonymous_authorization.rb
+++ lib/anonymous_authorization.rb
@@ -1,8 +1,9 @@
 require "anonymous_authorization/version"
-require "anonymous_authorization/engine"
 
 module AnonymousAuthorization
   def self.table_name_prefix
     "anon_"
   end
 end
+
+require "anonymous_authorization/engine"
